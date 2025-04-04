e92d9a36dacc92d8ae03a37a4653760283c2324b
Matheus R. Souza <matheusrian.dev@gmail.com>
2025-04-04 16:21:15 -0300

    Finalizando a aula 102 da Seção 3 do Curso

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Curso_Python_Udemy
+Curson de Python Completo - Luiz Otávio Miranda
+
+Cursado através da plataforma Udemy 
--- aula54-exercicio.py
+++ aula54-exercicio.py
@@ -36,21 +36,54 @@ contrário disso:
 # primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
 # print(f'O primeiro dígito do CPF é {primeiro_digito}')
 # Minha solução
+
+# Método 1 para remover caracteres indesejados
+#cpf_enviado = '746.824.890-70' \ # utilize o replace para remover caracteres simples como pontuações de alguns dados que precisam ser tratados. 
+    #.replace('.', '') \
+    #.replace(' ', '') \
+    #.replace('-', '')
+
+# Método 2 para remover caracteres indesejados
+import re
+import sys
+
 print('Sistema para Validação de CPF')
-cpf = input('Insira um CPF (Apenas números):')
+cpf_enviado = input('Insira um CPF válido:')
+cpf = re.sub( # basicamente o regex do c#
+    r'[^0-9]', # faz com que substitua tudo que não seja números de 0 a 9 pelo valor passado
+    '', # no caso, por nada, deixando apenas os valores numéricos.
+    cpf_enviado)
+
+# Método para recusar um cpf de um único número repetido 11 vezes.
+entrada_repetitiva = cpf == cpf[0] * len(cpf)
+if entrada_repetitiva:
+    print('Você digitou um valor sequencial (Ex.: 999.999.999-99)')
+    sys.exit()
+
+
 dez_digitos = cpf[:10]
 i = 11
 resultado = 0
+
 try:
+    if len(cpf) != 11: # Força IndexError caso a quantidade de caracteres passada for diferente de 11 caracteres
+        raise IndexError()
+
     for digito in dez_digitos:
         resultado += int(digito) * i
         i -= 1
     segundo_digito = (resultado * 10) % 11
     segundo_digito = segundo_digito if segundo_digito <= 9 else 0
+    # print(f'O segundo dígito do CPF é {segundo_digito}')
     
-    print(f'O segundo dígito do CPF é {segundo_digito}')
+    cpf_gerado = f'{cpf[:10]}{segundo_digito}'
+    if cpf_gerado == cpf:
+        print(f'O CPF {cpf_gerado} é válido.')
+    else:
+        print('CPF inválido.')
+    
 except ValueError:
-    print('Digite um CPF válido (apenas números)')
+    print('Digite um CPF válido')
 except IndexError:
     print('Quantidade de números inseridos não coincide com as de um CPF válido.')
 except:
