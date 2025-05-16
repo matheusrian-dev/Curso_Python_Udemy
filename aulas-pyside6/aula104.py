74ba44dd9184ed48f0333eefa4d55df84d3182f8
Matheus R. Souza <matheusrian.dev@gmail.com>
2025-05-16 16:54:27 -0300

    Explicação sobre snake_case, camelCase e PascalCase

---
+++ aulas-pyside6/aula104.py
@@ -0,0 +1,22 @@
+'''
+PascalCase, camelCase, snake_case: Qual usar?
+snake_case / camelCase: métodos, variáveis;
+PascalCase: Classes.
+
+Evite misturar padrões!
+Exemplo:
+
+contador_letras = 0
+contadorNumeros = 1
+
+Ambos acima são variáveis então o correto seria
+padronizar:
+
+contador_letras = 0
+contador_numeros = 0
+
+ou
+
+contadorLetras = 0
+contadorNumeros = 0
+'''
