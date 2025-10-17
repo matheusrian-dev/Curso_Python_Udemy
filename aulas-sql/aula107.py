1a8490142e23f0d85f5f0c952110e0a6dd89bca0
Matheus Rian <matheusrian.dev@gmail.com>
2025-10-16 22:29:55 -0300

    Exemplo de Inserts para inserir valor no db

--- aulas-sql/aula107.py
+++ aulas-sql/aula107.py
@@ -9,6 +9,12 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# Cuidado ao fazer delete sem where!
+cursor.execute(f'DELETE FROM {TABLE_NAME}')
+# Zera a sequência de Id's
+cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"')
+connection.commit()
+
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
@@ -17,6 +23,18 @@ cursor.execute(
     'weight REAL'
     ')'
 )
+connection.commit()
+
+# Cuidado com sql injection!
+# cursor.execute(
+#     f'INSERT INTO {TABLE_NAME} (name, weight) '
+#     'VALUES ("Luana", 57), '
+#     '("Geovanna", 63.5)'
+# )
+# Método menos perigoso de se inserir valores
+sql = f'INSERT INTO {TABLE_NAME} (name, weight) ' 'VALUES (?, ?)'
+# cursor.execute(sql, ['Geovanna', 63.2])
+cursor.executemany(sql, [['Geovanna', 63.2], ['Leah', 10.3], ['Aurora', 9.4]])
 connection.commit()
 
 cursor.close()
