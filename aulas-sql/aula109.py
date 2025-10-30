cbe16c5cb119abd5ebb98a15ad0009cb87d378c2
Matheus Rian <matheusrian.dev@gmail.com>
2025-10-30 00:06:09 -0300

    Aprendendo o básico sobre dotenv e criando a tabela customers

--- aulas-sql/aula109.py
+++ aulas-sql/aula109.py
@@ -1,13 +1,46 @@
+"""
+Python - dotenv
+
+A biblioteca python-dotenv (ou só dotenv) serve basicamente
+para guardar informações sensíveis ou de configuração em um
+arquivo separado (.env), em vez de deixar essas informações
+direto no código.
+"""
+
+import dotenv
 import pymysql
+import os
 
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+# Exemplo de como o dotenv puxa as variáveis de dentro do arquivo .env
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
-with connection:
-    with connection.cursor() as cursor:
 
-        # SQL
-        print(cursor)
+# As conexões são de uso único, após fechar não é possível
+# reutilizá-las.
+with connection:
+    # Já os cursores podem ser reutilizados.
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'name VARCHAR(50) NOT NULL, '
+            'age INT NOT NULL, '
+            'PRIMARY KEY (id)) '
+        )
+        # Atenção! O comando abaixo limpa a tabela informada!
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'INSERT INTO {TABLE_NAME} (name, age) VALUES ("Leah", 18) '
+        )
+    connection.commit()
