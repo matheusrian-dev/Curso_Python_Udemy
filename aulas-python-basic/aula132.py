fbd746a6b14d24fc7b39e9ca7a6711219a30ef46
Matheus R. Souza <matheusrian.dev@gmail.com>
2026-05-18 10:52:54 -0300

    Aula 132 - Criando o arquivo requirements.txt

---
+++ aulas-python-basic/aula132.py
@@ -0,0 +1,15 @@
+"""
+Gerando o arquivo requirements.txt
+
+É possível salvar as configurações armazenadas dentro da venv
+para exportação utilizando o comando abaixo:
+    pip freeze > nome_do_arquivo.txt
+
+Geralmente o nome utilizado é "requirements.txt".
+Com esse arquivo em mãos, é possível instalar dependências em
+outras máquinas de forma segura utilizando o comando abaixo:
+    pip install -r .\nome_do_arquivo.txt
+
+Lembre-se que é necessário atualizar o arquivo sempre que instalar
+novas dependências. Para isso basta repetir o primeiro comando acima.
+"""
