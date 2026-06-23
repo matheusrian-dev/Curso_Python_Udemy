03670c1cbf6e56822cf59e359b3e2ec649b14c76
Matheus Rian <matheusrian.dev@gmail.com>
2026-06-23 20:36:04 -0300

    Aula 142 - Positional-Only Parameters e Keyword-Only Arguments

---
+++ aulas-python-basic/aula142.py
@@ -0,0 +1,21 @@
+"""
+Positional-Only Parameters (/) e Keyword-Only Arguments (*)
+*args (ilimitado de argumentos posicionais)
+**kwargs (ilimitado de argumentos nomeados)
+🟢 Positional-Only Parameters (/) - Tudo antes da barra deve
+ser ! APENAS ! posicional.
+PEP 570 - Python Positional-Only Parameters
+https://peps.python.org/pep-0570/
+🟢 Keyword-Only Arguments (*) - * sozinho !NÃO SUGA! valores.
+Tudo que vier depois do * deve ser argumento nomeado.
+# PEP 3102 - Keyword-Only Arguments
+# https://peps.python.org/pep-3102/
+"""
+
+
+def soma(a, b, /, *, c, **kwargs):
+    print(kwargs)
+    print(a + b + c)
+
+
+soma(1, 2, c=3, nome='teste')
